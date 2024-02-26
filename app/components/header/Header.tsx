8c032bf4d8b41fdeb8c955733460abed13be5f9e
kshitijnair <kshitijnairr@gmail.com>
2024-02-25 18:37:19 -0800

    added tailwind working and image size options.

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,17 +1,19 @@
 import UserGroup from "./userGroup/UserGroup";
 import SearchBar from "./searchBar/SearchBar";
-import styles from './styles.module.css';
-import logo from "../../../public/logo.png"
+import styles from "./styles.module.css";
+import logo from "../../../public/logo.png";
 import Icon from "../icon/Icon";
 
 const Header = () => {
   return (
     <div className={styles.headerWrapper}>
-        <Icon src={logo.src} size='large' />
-        <SearchBar searchText="search here..." />
-        <UserGroup />
+      /** TODO: add classnames for components as props for custom styling from
+      parent components */
+      <Icon src={logo.src} size="large" />
+      <SearchBar searchText="search here..." />
+      <UserGroup />
     </div>
-  )
-}
+  );
+};
 
 export default Header;
