060af7cde05b38b82d384dba216b07aee6abc846
kshitijnair <kshitijnairr@gmail.com>
2024-03-02 16:34:36 -0800

    tried adding Link, failed :)

--- app/components/header/menuBar/MenuBar.tsx
+++ app/components/header/menuBar/MenuBar.tsx
@@ -1,16 +1,23 @@
-import styles from './styles.module.css'
+import Link from "next/link";
+import styles from "./styles.module.css";
 
 const MenuBar = () => {
   return (
     <div className={styles.menuBarContainer}>
-        <ul className={styles.menuBar}>
-            {/* <li className={styles.menuItem}>Home</li> */}
-            <li className={styles.menuItem}>Messages</li>
-            <li className={styles.menuItem}>Map</li>
-            <li className={styles.menuItem}>Settings</li>
-        </ul>
+      <ul className={styles.menuBar}>
+        {/* <li className={styles.menuItem}>Home</li> */}
+        <li className={styles.menuItem}>
+          <Link href={`/users`} />
+          Messages
+        </li>
+        <li className={styles.menuItem}>
+          <Link href={`/users`} />
+          Map
+        </li>
+        <li className={styles.menuItem}>Settings</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default MenuBar;
