fb39d04b99ad1463af3242aeb0f0fcc819d30a2f
kshitijnair <kshitijnairr@gmail.com>
2024-03-23 16:52:59 -0700

    pr

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ReduxProvider>{children}</ReduxProvider>
+        <ReduxProvider>
+          {children}
+        </ReduxProvider>
       </body>
     </html>
   );
